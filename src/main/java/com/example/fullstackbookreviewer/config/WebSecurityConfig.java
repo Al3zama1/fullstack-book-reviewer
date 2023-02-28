bae49f7280c7de2d9595e1146802ba72c79d0360
Abran Lezama <lezama.abran@gmail.com>
2023-02-28 10:43:14 -0600

    Project Setup
    - Implemented project configuration for cors, security, and WebClietn.
    - Added project database schemas.
    - Added configuration files for SQS and Keycloak.

---
+++ src/main/java/com/example/fullstackbookreviewer/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.fullstackbookreviewer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/api/v1/books").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/books/reviews").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/**").hasAuthority("ROLE_moderator")
+                        .requestMatchers("/api/**").authenticated())
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(new CustomAuthenticationConverter())));
+
+        return http.build();
+    }
+}
---
+++ src/test/java/com/example/fullstackbookreviewer/FullstackBookReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackbookreviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBookReviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
