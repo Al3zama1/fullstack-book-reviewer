2132afe63febf85cffa082493d173eda0c7e6f28
Abran Lezama <lezama.abran@gmail.com>
2023-02-28 11:39:09 -0600

    Fixed typo for findAllByOrderByCreatedAtDesc query method

--- src/main/java/com/example/fullstackbookreviewer/repository/ReviewRepository.java
+++ src/main/java/com/example/fullstackbookreviewer/repository/ReviewRepository.java
@@ -1,11 +1,11 @@
 package com.example.fullstackbookreviewer.repository;
 
 import com.example.fullstackbookreviewer.entity.Review;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
