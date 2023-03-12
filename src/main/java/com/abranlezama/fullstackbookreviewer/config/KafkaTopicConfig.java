9c46fbb4df4d38fd8ad0fab1710daccfd0f3205a
Abran Lezama <lezama.abran@gmail.com>
2023-03-11 17:05:59 -0800

    Added KafkaAdmin to configuration to stop kafka timeout error

--- src/main/java/com/abranlezama/fullstackbookreviewer/config/KafkaTopicConfig.java
+++ src/main/java/com/abranlezama/fullstackbookreviewer/config/KafkaTopicConfig.java
@@ -1,14 +1,30 @@
 package com.abranlezama.fullstackbookreviewer.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfig {
+
+    @Value("${messaging.kafka.bootstrap-servers}")
+    private String bootstrapServers;
     @Bean
     public NewTopic topic1() {
         return TopicBuilder.name("book-synchronization").build();
+    }
+
+    @Bean
+    KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
     }
 }
